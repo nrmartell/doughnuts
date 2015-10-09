69f0b5b7e60572a38fa537655173432fac436e0c
Natalie Martell <nrmartell@gmail.com>
2015-10-09 10:05:01 -0500

    add doughnuts works with console.log

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -1,4 +1,45 @@
 // jQuery Document Ready
 $(function() {
   
+ $('#addit').on('click', function(){
+    	var flavor = $('#doughnut-flavor').val();
+    	
+    	
+    	console.log("doughnut flavor: ", flavor);
+    	
+
+    	$.ajax({
+    		url:'http://api.doughnuts.ga/doughnuts',
+    		method: 'POST',
+    		datatype: 'json',
+    		data:{
+    			item: {
+
+    				flavor: flavor,
+    				
+    			}
+
+    		},
+    		done: function(data, textStatus, jqXHR){
+    			console.log('success!');
+    			console.log(data);
+
+    		},
+    		fail: function(){
+    			console.log('fail!');
+    			console.log(err);
+
+    		},
+    		always: function(){
+
+    			console.log('I always happen');
+    		}
+
+
+    	});
+
+    	return false;
+
+
+    });
 });
