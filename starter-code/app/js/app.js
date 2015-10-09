8a1686796dc82500ae9d2b73df9aca239f99d49e
Natalie Martell <nrmartell@gmail.com>
2015-10-09 13:40:11 -0500

    can append to list without refresh

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -1,51 +1,77 @@
 // jQuery Document Ready
 $(function() {
   
- $('#addit').on('click', function(){
-    	var flavor = $('#doughnut-flavor').val();
-    	var style  = $('#doughnut-style').val();
 
-    	
-    	
-    	console.log("doughnut flavor: ", flavor);
-    	console.log("doughnut style: ", style);
-    	$("#doughnuts").append(flavor).promise();
-    	$("#doughnuts").append(style).promise();
-    	
+var dArray= [];
 
-    	$.ajax({
-    		url:'http://api.doughnuts.ga/doughnuts',
-    		method: 'POST',
-    		datatype: 'json',
-    		data:{
-    			item: {
-
-    				flavor: flavor,
-    				style: style
-    				
-    			}
-
-    		},
-    		done: function(data, textStatus, jqXHR){
-    			console.log('success!');
-    			console.log(data);
-
-    		},
-    		fail: function(){
-    			console.log('fail!');
-    			console.log(err);
-
-    		},
-    		always: function(){
-
-    			console.log('I always happen');
-    		}
-
-
-    	});
-
-    	return false;
-
-
-    });
+$("#doughnut-list").click(function(){
+	dArray.forEach(function(doughnutsObj,index){
+	var flavor = doughnutsObj.flavor;
+	var style = doughnutsObj.style;
+	var yum = document.createElement("li");
+	$(yum).text(flavor + style);
+	$("#doughnuts")[0].appendChild(yum);
+	return false;
 });
+
+})
+
+$.ajax ({
+	url:"http://api.doughnuts.ga/doughnuts",
+	datatype: "jsonp",
+	method: "GET",
+	success: function (data){
+		for (var i =0; i < data.length; i++){
+			var flavor= data[i].flavor;
+			var style = data[i].style;
+			dArray.push({flavor: flavor, style: style});
+		}
+	} 
+});
+
+
+$('#addit').on('click', function(){
+	console.log("test");
+	var flavor = $('#doughnut-flavor').val();
+	var style  = $('#doughnut-style').val();
+
+	console.log("doughnut flavor: ", flavor);
+	console.log("doughnut style: ", style);
+	// dArray.push({flavor: flavor, style: style});
+	// $("#doughnuts").append('<li>'{flavor: flavor, style: style}'</li>');
+ //    $("#doughnuts").append('<li>'style'</li>');
+
+	$.ajax({
+		url:'http://api.doughnuts.ga/doughnuts',
+		method: 'POST',
+		datatype: 'json',
+		data:{
+			item: {
+				flavor: flavor,
+				style: style				
+			}
+		},
+		success: function(data){
+		$("#doughnuts").append('<li>'+ flavor + " "+ style +'</li>');
+		},
+		fail: function(){
+			console.log('fail!');
+			console.log(err);
+		},
+		always: function(){
+			console.log('I always happen');
+		}
+
+		
+
+			
+	});
+
+	});
+
+
+});
+
+
+
+    	
